348a933bb76f6846b5b8c83d5cfa617cc11f4de3
Dikesh.Chokshi <dikesh@cfl.rr.com>
2014-08-13 21:42:21 -0400

    game plan 8/13

---
+++ run_analysis.R
@@ -0,0 +1,8 @@
+run <- function () {
+  # 1. read 'train/X_train.txt': Training set.
+  # 2. read 'test/X_test.txt'
+  # 3. read features.txt -- apply these as columns name for the data sets.
+  # 4. merge 1 & 2
+  
+  
+}
