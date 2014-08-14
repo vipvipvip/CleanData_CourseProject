6367a431670079ad165d6ef699ec4038eb7ad5f2
Dikesh.Chokshi <dikesh@cfl.rr.com>
2014-08-13 21:49:58 -0400

    game plan

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,10 @@
 run <- function () {
-  # 1. read 'train/X_train.txt': Training set.
-  # 2. read 'test/X_test.txt'
-  # 3. read features.txt -- apply these as columns name for the data sets.
-  # 4. merge 1 & 2
+  # read 'train/X_train.txt': Training set.
+  # read 'test/X_test.txt'
+  # read features.txt -- apply these as columns name for the data sets.
+  #      remove _, -, (, ) from each column
+  # merge two data set now since col names are identical
+  # select columns with word "mean" and "sd"
   
   
 }
